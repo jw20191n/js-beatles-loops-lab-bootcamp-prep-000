4748eba4bddb7b0d5eecf258d5a2c2e24d0ccbc3
Jiayi Wang <clairejw1015@gmail.com>
2019-04-24 00:25:29 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // add solution here
 function theBeatlesPlay(musicians,instruments){
   var musicPlay = []
-  for (let i=0;i<musicians;i++){
+  for (let i=0;i<musicians.length;i++){
     note = musicians[i] + "plays" + instruments[i]
     musicPlay.push(note) 
   }
