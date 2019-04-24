5cf7d6a9d6bc20f1eb49c0b88c76d58f030f09d7
Jiayi Wang <clairejw1015@gmail.com>
2019-04-24 00:22:05 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 // add solution here
 function theBeatlesPlay(musicians,instruments){
   var musicPlay = []
-  
+  for (let i=0;i<musicians;i++){
+    musicPlay[i] = musicians[i] + "plays" + instruments[i]
+  }
+  return musicPlay
 }
